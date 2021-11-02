fe45791d7f8e9358e86aef0e4588cf9816418b1f
victoralfonzo <vaa@andrew.cmu.edu>
2021-11-02 18:21:39 -0400

    simplified model to use baseline features and alcohol consumption

--- dockerfile/apps/app.py
+++ dockerfile/apps/app.py
@@ -1,9 +1,7 @@
 from flask import Flask, jsonify, request
-from sklearn.preprocessing import LabelEncoder
 import joblib
 import pandas as pd
 import numpy as np
-
 
 app = Flask(__name__)
 
@@ -14,40 +12,18 @@ def hello():
 @app.route('/predict')
 def predict():
 	 #use entries from the query string here but could also use json
-     assert(len(request.args) == 30, "Please provide all 30 features") 
-     request.args={
-         "school":"GP",
-         "sex":"F",
-         "age":15,
-         "address":"U",
-         "famsize":"LE3",
-         "Pstatus":"T",
-         "Medu":0,
-         "Fedu":0,
-         "Mjob":"teacher",
-         "FJob":"teacher",
-         "reason":"home",
-         "guardian":"mother",
-         "traveltime":1,
-         "studytime":1,
-         "failures":1,
-         "schoolsup":True,
-         "famsup":True,
-         "paid":True,
-         "activities":True,
-         "nursery":True,
-         "higher":True,
-         "internet":True,
-         "romantic":True,
-         "famrel":1,
-         "freetime":1,
-         "goout":1,
-         "Dalc":1,
-         "Walc":1,
-         "health":1,
-         "absences":0
-     }
-     query_df = pd.DataFrame(request.args, index=[0])
+     age = request.args.get('age')
+     absences = request.args.get('absences')
+     health = request.args.get('health')
+     walc = request.args.get('Walc')
+     dalc = request.args.get('Dalc')
+     data = [[age],[health],[absences], [walc], [dalc]]
+     query_df = pd.DataFrame({ 'age' : pd.Series(age)
+                            ,'health' : pd.Series(health) ,
+                            'absences' : pd.Series(absences),
+                            'Walc': pd.Series(walc),
+                            'Dalc' : pd.Series(dalc),
+                            })
      query = pd.get_dummies(query_df)
      prediction = clf.predict(query)
      return jsonify(np.asscalar(prediction))
